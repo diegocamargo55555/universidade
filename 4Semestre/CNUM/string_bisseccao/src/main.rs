fd3283665d6ebd36f4f37d8a8ee827f3b5f27793
diego <diegocamargo55555@gmail.com>
2024-09-01 23:36:55 -0300

    realiza operacao e retira os numeros

--- 4Semestre/CNUM/string_bisseccao/src/main.rs
+++ 4Semestre/CNUM/string_bisseccao/src/main.rs
@@ -1,6 +1,4 @@
 
-
-
 fn main() {
     //let a = 5.0;
     //let b = 8.0;
@@ -9,43 +7,63 @@ fn main() {
 
 
     let mut eq1 = String::from("$9 + 5 - 7 + 5 + 6 -8 $");
-
-    let s =remove_whitespace(&mut eq1); // 9+5-7+5+6-8
-
-
-
-
     //let eq2 = String::from("$x + 2 * 3$");
     //let eq3 = String::from("$x * 3 + 2$");
     //let eq4 = String::from("$x * (3 + 2)$");
     //let eq2 = String::from("");
     //let eq2 = String::from("");
+    
 
-    println!("{}", s);
-
-    let menos: usize = s.chars().position(|c|c == '-').unwrap();
-    let mais = s.chars().position(|c|c == '+').unwrap();
-    let cifrao = s.chars().position(|c|c == '$').unwrap();
-
-    let mut v = vec![cifrao, menos, mais];
-    v.sort();
-
-    let ch = s.chars().nth(v[1]).unwrap();
-
-    let s1= &s[v[0]+1..v[1]];
-    let n1: f64 =  s1.parse().unwrap();
-
-    let s2 = &s[v[1]+1..v[2]];
-    let n2: f64 =  s2.parse().unwrap();
-
-    println!("n1:{} \nn2:{}", n1, n2);
-
-    resultado = operacao(ch, n1, n2);
+        let mut s =remove_whitespace(&mut eq1); // 9+5-7+5+6-8
+        println!("inicio: {}", s);
 
 
-    println!("valor:{}", resultado);
+        let mut menos: usize = s.chars().position(|c|c == '-').unwrap();
+        let mut mais = s.chars().position(|c|c == '+').unwrap();
+        let mut cifrao = s.chars().position(|c|c == '$').unwrap();
+
+        let mut v = vec![cifrao, menos, mais];
+        v.sort();
+
+        let ch = s.chars().nth(v[1]).unwrap();
+
+        //pega os numeros que serão usados na operação
+        let mut s1= &s[v[0]+1..v[1]];
+        let mut n1: f64 =  s1.parse().unwrap();
+        let mut s2 = &s[v[1]+1..v[2]];
+        let mut n2: f64 =  s2.parse().unwrap();
+        //println!("n1:{} \nn2:{}", n1, n2);
+
+        resultado = operacao(ch, n1, n2);
+        let mut resultadoSTR= resultado.to_string();
+
+        s.replace_range(v[1]+1..v[1]+2, &resultadoSTR);
+        s.replace_range(v[0]..v[1]+1, " ");
+
+        println!("tes2: {}", s);
+
+
+        println!("valor2:{}", resultado);
+
+        /////////////
+
+        
 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
@@ -64,9 +82,3 @@ fn operacao(ch: char, n1: f64, n2: f64) -> f64{
     
 }
 
-/*
-    for x in v {
-        println!("{}", x)
-    }
-
-*/
