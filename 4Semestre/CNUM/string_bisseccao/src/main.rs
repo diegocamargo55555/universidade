ad5027892a4179b923fc51c80802937324bece11
diego <diegocamargo55555@gmail.com>
2024-09-01 22:40:36 -0300

    somando e subtraindo os 2 primeiros numeros

--- 4Semestre/CNUM/string_bisseccao/src/main.rs
+++ 4Semestre/CNUM/string_bisseccao/src/main.rs
@@ -9,7 +9,11 @@ fn main() {
 
 
     let mut eq1 = String::from("$9 + 5 - 7 + 5 + 6 -8 $");
+
     let s =remove_whitespace(&mut eq1); // 9+5-7+5+6-8
+
+
+
 
     //let eq2 = String::from("$x + 2 * 3$");
     //let eq3 = String::from("$x * 3 + 2$");
@@ -19,18 +23,14 @@ fn main() {
 
     println!("{}", s);
 
-    let menos: usize = (s.chars().position(|c|c == '-').unwrap()) ;
+    let menos: usize = s.chars().position(|c|c == '-').unwrap();
     let mais = s.chars().position(|c|c == '+').unwrap();
     let cifrao = s.chars().position(|c|c == '$').unwrap();
 
     let mut v = vec![cifrao, menos, mais];
     v.sort();
 
-    let op= &s[v[1]..v[2]];
-    println!("{}", op);
-
-    
-
+    let ch = s.chars().nth(v[1]).unwrap();
 
     let s1= &s[v[0]+1..v[1]];
     let n1: f64 =  s1.parse().unwrap();
@@ -40,19 +40,27 @@ fn main() {
 
     println!("n1:{} \nn2:{}", n1, n2);
 
-    resultado = n1 + n2;
+    resultado = operacao(ch, n1, n2);
 
 
-    //println!("valor:{}", resultado);
-
+    println!("valor:{}", resultado);
 
     
+
 }
 
 fn remove_whitespace(s: &str) -> String {
     s.chars().filter(|c| !c.is_whitespace()).collect()
 }
-fn verOperacao(){
+fn operacao(ch: char, n1: f64, n2: f64) -> f64{
+    if ch == '+' {
+        return n1 + n2;
+    }
+    else if ch == '-' {
+        return n1 - n2;        
+    }
+
+    return 0.0;
     
 }
 
