9f350d6c48f60da39a54a7781fb595bcffdd22c4
diegoC <diegocamargo55555@gmail.com>
2024-09-04 13:44:30 -0300

    add librarys: op.rs e sum_sub.rs

--- 4Semestre/CNUM/string_bisseccao/target/debug/deps/string_bisseccao-8a387b97fac1bba8.d
+++ 4Semestre/CNUM/string_bisseccao/target/debug/deps/string_bisseccao-8a387b97fac1bba8.d
@@ -1,5 +1,7 @@
-/home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/target/debug/deps/string_bisseccao-8a387b97fac1bba8: src/main.rs
+/home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/target/debug/deps/string_bisseccao-8a387b97fac1bba8: src/main.rs src/op.rs src/sum_sub.rs
 
-/home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/target/debug/deps/string_bisseccao-8a387b97fac1bba8.d: src/main.rs
+/home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/target/debug/deps/string_bisseccao-8a387b97fac1bba8.d: src/main.rs src/op.rs src/sum_sub.rs
 
 src/main.rs:
+src/op.rs:
+src/sum_sub.rs:
--- 4Semestre/CNUM/string_bisseccao/target/debug/string_bisseccao.d
+++ 4Semestre/CNUM/string_bisseccao/target/debug/string_bisseccao.d
@@ -1,1 +1,1 @@
-/home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/target/debug/string_bisseccao: /home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/src/main.rs
+/home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/target/debug/string_bisseccao: /home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/src/main.rs /home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/src/op.rs /home/diego/Documents/universidade/4Semestre/CNUM/string_bisseccao/src/sum_sub.rs
