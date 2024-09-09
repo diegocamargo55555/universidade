9a0605f8ea4eb1afbd8864090edfb5b27d7ff029
diegoC <diegocamargo55555@gmail.com>
2024-09-09 16:02:18 -0300

    Bug quando se mutiplica um numero por um numero negativo

--- 4Semestre/CNUM/string_bisseccao/src/main.rs
+++ 4Semestre/CNUM/string_bisseccao/src/main.rs
@@ -6,8 +6,8 @@ use crate::mul_div::mult_div;
 
 
 fn main() {
-    let x = "9";
-    let mut eq1: String = String::from("$x + 55 - 77 + 5 + 60 -8 $");
+    let x = "-2";
+    let mut eq1: String = String::from("$57*x/4.56 + 12*x*12*x*4/3*x + 0*0 - 0*x $");
 
     
     let mut eq2 = String::from("$x * 2 * 3 * 77 - 3* 99 *5 +23 +56$");
@@ -16,7 +16,7 @@ fn main() {
     //let eq4 = String::from("$x * (3 + 2)$");
 
     let mut eqs: String = op::remove_whitespace(&mut eq1); // 9+5-7+5+6-8
-    println!("eq inicial: {}", eq2);
+    println!("eq inicial: {}", eq1);
     eqs = eqs.replace("x", &x);
 
     eqs = mult_div(eqs);
