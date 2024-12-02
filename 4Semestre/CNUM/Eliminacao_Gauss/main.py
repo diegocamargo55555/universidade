de5bf6c00b010f03fd3f0a2cf771d08f5eacee55
diegoC <diegocamargo55555@gmail.com>
2024-12-02 03:40:57 -0300

    CNUM GAUSS Elimination acho que completa

--- 4Semestre/CNUM/Eliminacao_Gauss/main.py
+++ 4Semestre/CNUM/Eliminacao_Gauss/main.py
@@ -2,87 +2,92 @@ import numpy as np
 
 def print_matriz(linha, col, matrix):
     print("------")
-    for row in range(linha):
-        for column in range(col):
-            print("{:.4f}".format(matrix[row][column]), end=" ")
-        print()
-
-
-
-#linha = int(input("insira quantas linhas a matrix terá "))
-#col = int(input("insira quantas colunas a matrix terá "))
-#matrix = []
-#print("insira o valor de:")   
-#for l in range(linha):    #3 1 4 5
-#    a = []
-#    for c in range(col):
-#        a.append(float(input("A{}{}: " .format(l+1, c+1))))
-#    matrix.append(a)
-#
-
-
-# APENAS PARA TESTES
-linha = int(3)
-col = linha+1
-matrix = np.array([[3,2,1,6], [4,5,6,8], [9,8,11,9]])
-
-#linha = int(3)
-#col = int(3)
-#matrix = [[3,2,1], [4,5,6], [9,8,11], [11,22,35]]
-
-# APENAS PARA TESTES
-for i in range(linha-1):
-    print("\n\n")
-    print_matriz(linha, col, matrix)
-    li = 0
-    #while li < linha-1:
-    for li in range(linha-1):
-        print("li:", li)
-        l = li + i
-        print("i:{}   l:{}".format(i,l+1))
-        print(l)
-        if l+1 >= linha:
-            print("break")
-            break
-        
-        if matrix[i][i] == 0:
-            ii = i
-            for _ in range(linha - i):
-                ii += 1
-                if matrix[i][i] != 0:
-                    matrix [[i,ii]] = matrix[[ii,i]]
-                    break
-                    
-                    
-        m = matrix[l+1][i] / matrix[i][i]
-        print("m = ", matrix[l+1][i], "/",matrix[i][i] , "\nm: ",m)
-
+    for l in range(linha):
         for c in range(col):
-            print("A{}{} = {} - {}*{}".format(l+1, c, matrix[l+1][c], m ,matrix[i][c]))
-            matrix[l+1][c] = matrix[l+1][c] - m*matrix[i][c]
+            print("{:.4f}".format(matrix[l][c]), end=" ")
+        print()
         
+def main():
+
+
+    #linha = int(input("insira quantas linhas a matrix terá "))
+    #col = linha+1
+    #matrix = []
+    #print("insira o valor de:")   
+    #for l in range(linha):    #3 1 4 5
+    #    a = []
+    #    for c in range(col):
+    #        a.append(float(input("A{}{}: " .format(l+1, c+1))))
+    #    matrix.append(a)
+    #
+
+    # APENAS PARA TESTES
+    linha = int(3)
+    col = linha+1
+    matrix = np.array([[0,2,1,6], [4,5,6,8], [9,8,11,9]], dtype=np.longdouble)
+
+    #linha = int(3)
+    #col = linha+1
+    #matrix = [[3,2,1], [4,5,6], [9,8,11], [11,22,35]]
+
+    mat = np.array(matrix[:, :-1], dtype=np.double)
+    det = np.linalg.det(mat) 
+    print("determinante = ", det)
+
+    #if det == 0: 
+    #    print("nao é possivel resolver sistemas de equações lineares co metodo de Gauss")
+    #    return 0
+    #
+    for i in range(linha-1): 
+        print("\n\n iteração", i)
         print_matriz(linha, col, matrix)
+        li = 0
+        #while li < linha-1:
+        for li in range(linha-1):
+            l = li + i
+            if l+1 >= linha:
+                print("break")
+                break
+            
+            if matrix[i][i] == 0: #troca linha
+                ii = i
+                print("troca linhas")
+                for _ in range(linha - i):
+                    ii += 1
+                    if matrix[ii][i] != 0:
+                        matrix [[i,ii]] = matrix[[ii,i]]
+                        print_matriz(linha, col, matrix)
+                        break
 
-x = np.zeros(linha)
-
-x[linha-1] = matrix[linha-1][linha]/matrix[linha-1][linha-1]
-print("\n\n\n")
-print(x[linha-1])
-
-for i in range(linha-2,-1,-1):
-    x[i] = matrix[i][linha]
-
-    for j in range(i+1,linha):
-        x[i] = x[i] - matrix[i][j]*x[j]
-
-    x[i] = x[i]/matrix[i][i]
-    print(x[i])
-
-
-print('\nRequired solution is: ')
-for i in range(linha):
-    print('X%d = %0.2f' %(i,x[i]), end = '\t')
-
-
+            m = matrix[l+1][i] / matrix[i][i]
+            print("m = ", matrix[l+1][i], "/",matrix[i][i] , "\nm: ",m)
 
 
+            for c in range(col):# calcula o novo valor da linha
+                print("A{}{} = {} - {}*{} = {}".format(l+1, c, matrix[l+1][c], m ,matrix[i][c], (matrix[l+1][c] - m*matrix[i][c])))
+                matrix[l+1][c] = matrix[l+1][c] - m*matrix[i][c]
+
+            print_matriz(linha, col, matrix)
+
+    x = np.zeros(linha)
+
+    x[linha-1] = matrix[linha-1][linha]/matrix[linha-1][linha-1]
+    print("\n\n\n")
+    print(x[linha-1])
+
+    for i in range(linha-2,-1,-1):
+        x[i] = matrix[i][linha]
+
+        for j in range(i+1,linha):
+            x[i] = x[i] - matrix[i][j]*x[j]
+
+        x[i] = x[i]/matrix[i][i]
+        print(x[i])
+
+
+    print('\nSolução: ')
+    for i in range(linha):
+       print('X%d = %0.2f' %(i,x[i]), end = '\t')
+
+
+main()
--- 4Semestre/CNUM/Eliminacao_Gauss/test.py
+++ 4Semestre/CNUM/Eliminacao_Gauss/test.py
@@ -1,47 +1,13 @@
-# Importing NumPy Library
 import numpy as np
-import sys
 
-# Reading number of unknowns
-n = int(input('Enter number of unknowns: '))
+linha = int(3)
+col = linha+1
+matrix = np.array([[0,2,1,6], [4,5,6,8], [9,8,11,9]], dtype=np.longdouble)
 
-# Making numpy array of n x n+1 size and initializing 
-# to zero for storing augmented matrix
-a = np.zeros((n,n+1))
+#linha = int(3)
+#col = linha+1
+#matrix = [[3,2,1], [4,5,6], [9,8,11], [11,22,35]]
+mat = np.array(matrix[:, :-1], dtype=np.longdouble)
 
-# Making numpy array of n size and initializing 
-# to zero for storing solution vector
-x = np.zeros(n)
-
-# Reading augmented matrix coefficients
-print('Enter Augmented Matrix Coefficients:')
-for i in range(n):
-    for j in range(n+1):
-        a[i][j] = float(input( 'a['+str(i)+']['+ str(j)+']='))
-
-# Applying Gauss Elimination
-for i in range(n):
-    if a[i][i] == 0.0:
-        sys.exit('Divide by zero detected!')
-        
-    for j in range(i+1, n):
-        ratio = a[j][i]/a[i][i]
-        
-        for k in range(n+1):
-            a[j][k] = a[j][k] - ratio * a[i][k]
-
-# Back Substitution
-x[n-1] = a[n-1][n]/a[n-1][n-1]
-
-for i in range(n-2,-1,-1):
-    x[i] = a[i][n]
-    
-    for j in range(i+1,n):
-        x[i] = x[i] - a[i][j]*x[j]
-    
-    x[i] = x[i]/a[i][i]
-
-# Displaying solution
-print('\nRequired solution is: ')
-for i in range(n):
-    print('X%d = %0.2f' %(i,x[i]), end = '\t')
+print(matrix, "\n")
+print(mat)
