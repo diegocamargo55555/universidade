4e3a368eb36a496b0f0862fd06da212f42fa231a
diegocamargo55555 <diegocamargo55555@gmail.com>
2025-03-26 15:52:29 -0300

    tratamento de linha incompleto

--- 5Semestre/Pesquisa_OP/read_file/src/main.rs
+++ 5Semestre/Pesquisa_OP/read_file/src/main.rs
@@ -2,25 +2,28 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 mod tratar_linha;
+use crate::io::BufReader;
+use tratar_linha::contar_x;
 use tratar_linha::tirarX;
-
 fn main() {
-    // File hosts.txt must exist in the current path
-    if let  Ok(lines) = read_lines("/home/heilt/Documents/universidade/5Semestre/Pesquisa_OP/read_file/src/novo.txt") {
-        // Consumes the iterator, returns an (Optional) String
+    let total_x = contar_x;
+    if let Ok(lines) = read_lines(
+        "/home/heilt/Documents/universidade/5Semestre/Pesquisa_OP/read_file/src/novo.txt",
+    ) {
         let mut x = 1;
         for mut line in lines.map_while(Result::ok) {
             line = tirarX(line.clone());
-            println!("eq:{} {}",x, line);
-            x = x+1;
+            println!("eq{}: \t {}", x, line);
+            x = x + 1;
         }
     }
+
 }
 
-// The output is wrapped in a Result to allow matching on errors.
-// Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
--- 5Semestre/Pesquisa_OP/read_file/src/tratar_linha.rs
+++ 5Semestre/Pesquisa_OP/read_file/src/tratar_linha.rs
@@ -1,13 +1,52 @@
-pub fn tirarX(line: String) -> String{
-    let mut x: usize = line.find("x").unwrap();
-    println!("{}", x);
+use std::fs::File;
+use std::io::BufRead;
+use crate::io::BufReader;
+use crate::read_lines;
 
-    let mut single = &line[x..x+1];
-    while single != " "{
-        
-        x = x+1;
-        single = &line[x..x+1];
+pub fn tirarX(mut line: String) -> String {
+
+    while line.contains("x") {
+        let mut x: usize = line.find("x").unwrap();
+
+        while line[x..x + 1] != *" " {
+            line.replace_range(x..x + 1, "1");
+
+            line.replace_range(x + 1..x + 2, " ");
+
+            x = x + 1;
+        }
     }
-    println!("{}", line);
     return line;
+}
+
+fn contX(line: String) -> usize {
+    if line.contains("max") {
+        return line.chars().filter(|c| *c == 'x').count() - 1;
+    }
+    return line.chars().filter(|c| *c == 'x').count();
+}
+
+pub fn contar_x() {
+    if let Ok(lines) = read_lines("/home/heilt/Documents/universidade/5Semestre/Pesquisa_OP/read_file/src/novo.txt",) {
+        // Consumes the iterator, returns an (Optional) String
+        let mut total_x = 0;
+        for line in lines.map_while(Result::ok) {
+            let temp = contX(line.clone());
+            if total_x < temp {
+                total_x = temp;
+            }
+        }
+        println!("total de X:{}", total_x)
+    }
+}
+pub fn preenchex(){
+    let f = File::open("/home/heilt/Documents/universidade/5Semestre/Pesquisa_OP/read_file/src/novo.txt").unwrap();
+    let mut reader = BufReader::new(f);
+    let mut number_of_lines: String = String::new();
+    reader.read_line(&mut number_of_lines);
+
+    let totalx = contX(number_of_lines.clone());
+    if number_of_lines.contains("max") || number_of_lines.contains("min") {
+        
+    }
 }
