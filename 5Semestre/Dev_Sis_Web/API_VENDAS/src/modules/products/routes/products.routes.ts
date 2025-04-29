4d125286f85f842cc5697a995014e62d288fbdab
diegocamargo55555 <diegocamargo55555@gmail.com>
2025-04-29 17:32:17 -0300

    dev web create user

--- 5Semestre/Dev_Sis_Web/API_VENDAS/src/modules/products/routes/products.routes.ts
+++ 5Semestre/Dev_Sis_Web/API_VENDAS/src/modules/products/routes/products.routes.ts
@@ -7,15 +7,15 @@ const productsController = new ProductsController();
 
 productsRouter.get('/', async (req, res, next) => {
     try {
-        console.log("1")
         await productsController.index(req, res, next)
     } catch (err) {
         next(err)
     }
 })
 
-productsRouter.get('/:id', async (req, res, next) => {
-    console.log("2")
+productsRouter.get('/:id', celebrate({
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() }
+}), async (req, res, next) => {
     try {
         await productsController.show(req, res, next)
     } catch (err) {
@@ -23,46 +23,52 @@ productsRouter.get('/:id', async (req, res, next) => {
     }
 })
 
-productsRouter.post('/', async (req, res, next) => {
-    console.log("3")
-    try {
-        await productsController.create(req, res, next)
-    } catch (err) {
-        next(err)
-    }
-})
+productsRouter.post('/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().precision(2).min(0).required(),
+            quantity: Joi.number().min(0).required()
+        }
+    }),
+    async (req, res, next) => {
+        try {
+            await productsController.create(req, res, next)
+        } catch (err) {
+            next(err)
+        }
+    })
 
-productsRouter.put('/:id', async (req, res, next) => {
-    console.log("4")
+productsRouter.put('/:id',
+    celebrate({
+        [Segments.PARAMS]: { id: Joi.string().uuid().required() },
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().precision(2).min(0).required(),
+            quantity: Joi.number().min(0).required()
+        }
+    }),
+    async (req, res, next) => {
 
-    try {
-        await productsController.update(req, res, next)
-    } catch (err) {
-        next(err)
-    }
-})
+        try {
+            await productsController.update(req, res, next)
+        } catch (err) {
+            next(err)
+        }
+    })
 
 
-productsRouter.delete('/:id', async (req, res, next) => {
-    console.log("5")
+productsRouter.delete('/:id', celebrate({
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() }
+}),
+    async (req, res, next) => {
+        try {
+            await productsController.delete(req, res, next)
+        } catch (err) {
+            next(err)
+        }
+    })
 
-    try {
-        await productsController.delete(req, res, next)
-    } catch (err) {
-        next(err)
-    }
-})
 
-productsRouter.get('/:id', celebrate({
-    [Segments.PARAMS] : {id: Joi.string().uuid().required()}
-  }),
-  async (req, res, next) => {
-    try {
-      await productsController.show(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  });
-  
 
 export default productsRouter
--- 5Semestre/Dev_Sis_Web/API_VENDAS/src/shared/http/routes/index.ts
+++ 5Semestre/Dev_Sis_Web/API_VENDAS/src/shared/http/routes/index.ts
@@ -1,7 +1,6 @@
 import productsRouter from "@modules/products/routes/products.routes";
 import { Router } from "express";
 const routes = Router();
-console.log("65")
 
 routes.use('/products', productsRouter);
 
