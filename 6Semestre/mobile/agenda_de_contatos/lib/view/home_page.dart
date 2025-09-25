3ada02e0c3be75e63b71a34c2f87f5cb77d9016b
DiNT <diegocamargo55555@gmail.com>
2025-09-25 11:27:48 -0300

    linguagem satanica - agenda de contatos

---
+++ 6Semestre/mobile/agenda_de_contatos/lib/view/home_page.dart
@@ -0,0 +1,112 @@
+import 'dart:io';
+
+import 'package:agenda_de_contatos/database/helper/contact_helper.dart';
+import 'package:agenda_de_contatos/database/model/contact_model.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ContactHelper helper = ContactHelper();
+  List<Contact> contacts = [];
+  @override
+  void initState() {
+    super.initState();
+    // Contact c = Contact(
+    //   name: "Ana Beatriz",
+    //   email: "aanaaaa@email.com",
+    //   phone: "44444",
+    //   img: null,
+    // );
+    // helper.saveContact(c);
+    // helper.getAllContacts().then((list) {
+    //   print(list);
+    // });
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Agenda de Contatos",
+          style: TextStyle(color: const Color.fromARGB(255, 252, 254, 255)),
+        ),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      backgroundColor: const Color.fromARGB(255, 252, 254, 255),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsetsGeometry.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return _contactCard(context, index);
+        },
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsetsGeometry.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null
+                        ? FileImage(File(contacts[index].img!))
+                        : AssetImage("assets/imgs/images.png") as ImageProvider,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      contacts[index].name ?? "",
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      contacts[index].email ?? "",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    Text(
+                      contacts[index].phone ?? "",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
